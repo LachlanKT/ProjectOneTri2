d411eb704a332079ebdbc60c74befbcbc360d4b3
LACHLAN\lachl <1032038@student.sae.edu.au>
2024-02-12 21:19:17 +1030

    no message

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -9,22 +9,44 @@ public class Main : MonoBehaviour
     public int playerExp = 0;
     public int playerExpThreshold = 0;
     public int enemyLevel = 0;
-    public int enemyHealth = 0;
+    public float enemyHealth = 10;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        for (int level = 1; level <= playerLevel; level++)
+        for (int level = 2; level <= playerLevel; level++)
         {
             playerStrength *= 1.25f;
             playerStrength = (int)playerStrength; // round down
         }
         // playerStrength = (int)(10 * (1 + (playerLevel - 1) * 0.25f));
-        Debug.Log(playerStrength);
+        Debug.Log("Players strength equals: " + playerStrength);
+
+        EnemyAppears();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space)) 
         
+        { 
+        
+        }
+    }
+
+    public void EnemyAppears() 
+    {
+        enemyLevel = Random.Range(1, 11);
+        Debug.Log("Enemy level equals: " + enemyLevel);
+
+        for (int checkEnemyLevel = 2; checkEnemyLevel <= enemyLevel; checkEnemyLevel++)
+        {
+            enemyHealth *= 1.25f;
+            enemyHealth = (int)enemyHealth; // round down
+        }
+        Debug.Log("EnemyHealth equals: " + enemyHealth);
     }
 }
